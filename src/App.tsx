d38e468adfb488f4004158b7b3e5a9f0a41a5268
aDp08 <149078560+aDp08@users.noreply.github.com>
2024-10-20 22:23:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import { AppContextProvider } from "./Context/AppContext";
 import ImageCart from "./Components/ImageCart";
 import ImageUploader from "./Components/ImageUploader";
+import DatabasePage from "./Components/database"; // Import the new DatabasePage component
 
 const App = () => {
     return (
@@ -12,7 +12,8 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<ImageCart />} />
-                    <Route path="/upload" element={<div><ImageUploader /></div>} />
+                        <Route path="/upload" element={<div><ImageUploader /></div>} />
+                        <Route path="/database" element={<DatabasePage />} /> {/* New route for Database */}
                     </Route>
                 </Routes>
             </Router>
